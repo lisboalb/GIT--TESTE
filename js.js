41074b656838e4798a2947ef10a58a7087e35a27
lisboalb <lucaslb.adm@gmail.com>
2024-10-30 20:21:09 -0300

    Modificaçõe em jsjs

--- js.js
+++ js.js
@@ -1,7 +1,20 @@
-function somar(a,b ) {
+/*function somar(a,b ) {
     let result = (a + b)
 }
 
 let numeros = 18 + 7
 
 document.write(`Resultados ${numeros}`)
+
+*/
+
+
+ function maior (P1, P2){
+    if(P1 >= P2)  {
+        return P1
+    } else {
+        return P2
+    }
+ }
+
+ console.log(45,10)
